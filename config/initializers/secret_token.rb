a4137590549cd54dcfb6d6be514bbdb8c52235c7
Henry Hund <henryhund@gmail.com>
2013-06-21 14:32:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lorious::Application.config.secret_token = '037eb1f9f73695ab922b65b7cbc7d776e5b6b04ff3ea53a2aa4533ad62ef29c91ae4b7e94b6e168ffe8cf09bcc6bc558962da0824f2e94c8cc18a357988b56de'
